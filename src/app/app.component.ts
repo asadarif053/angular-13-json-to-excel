efd1bf79f8b5dd6f582d9d7a44867f3f90dd0d3b
Asaduzzaman <asadarif053@gmail.com>
2022-10-12 18:01:53 +0600

    "Angular 13 req: @types/file-saver"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ExcelService} from './services/excel.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'excel-test';
+  title = 'excel app';
+
+
+  data: any = [{
+    eid: 'e101',
+    ename: 'ravi',
+    esal: 1000
+  },
+  {
+    eid: 'e102',
+    ename: 'ram',
+    esal: 2000
+  },
+  {
+    eid: 'e103',
+    ename: 'rajesh',
+    esal: 3000
+  }];
+
+  constructor(private excelService:ExcelService){
+    
+  }
+  exportAsXLSX():void {
+    this.excelService.exportAsExcelFile(this.data, 'sample');
+  }
+
 }
